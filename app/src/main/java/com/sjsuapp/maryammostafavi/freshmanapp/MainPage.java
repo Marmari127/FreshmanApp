497b4036fabc316bb8677d9c2bdae96e5d37ba17
Marmari127 <36213942+Marmari127@users.noreply.github.com>
2018-04-05 06:19:55 -0700

    Update MainPage.java

--- app/src/main/java/com/sjsuapp/maryammostafavi/freshmanapp/MainPage.java
+++ app/src/main/java/com/sjsuapp/maryammostafavi/freshmanapp/MainPage.java
@@ -2,13 +2,46 @@ package com.sjsuapp.maryammostafavi.freshmanapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainPage extends AppCompatActivity {
 
+    TextView textView;
+    Button tipsButton;
+    Button mapsButoon;
+    Button resourcesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        textView = (TextView) findViewById(R.id.textView);
+
+        tipsButton = findViewById(R.id.tips_button);
+        tipsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               setContentView(R.layout.tips);
+
+            }
+        });
+
+        mapsButoon = findViewById(R.id.maps_button);
+        mapsButoon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_maps);
+            }
+        });
+        resourcesButton = findViewById(R.id.resources_button);
+        resourcesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.resources);
+            }
+        });
+
     }
 
 }
