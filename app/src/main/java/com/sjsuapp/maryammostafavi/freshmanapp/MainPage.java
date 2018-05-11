db147048d010561208542efc57c26e5025e2066b
sjaime97 <38027776+sjaime97@users.noreply.github.com>
2018-05-10 17:07:53 -0700

    Update MainPage.java

--- app/src/main/java/com/sjsuapp/maryammostafavi/freshmanapp/MainPage.java
+++ app/src/main/java/com/sjsuapp/maryammostafavi/freshmanapp/MainPage.java
@@ -1,47 +1,46 @@
-package com.sjsuapp.maryammostafavi.freshmanapp;
+package com.example.samanthajaime.freshmanapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.content.Intent;
 
-public class MainPage extends AppCompatActivity {
-
-    TextView textView;
-    Button tipsButton;
-    Button mapsButoon;
-    Button resourcesButton;
+//This class includes all the java code for the main menu buttons to go onto
+//new pages
+public class MainPage extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        textView = (TextView) findViewById(R.id.textView);
 
-        tipsButton = findViewById(R.id.tips_button);
-        tipsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               setContentView(R.layout.tips);
-
-            }
-        });
-
-        mapsButoon = findViewById(R.id.maps_button);
-        mapsButoon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_maps);
-            }
-        });
-        resourcesButton = findViewById(R.id.resources_button);
-        resourcesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.resources);
-            }
-        });
-
+        setTitle("SJSU Freshman App");
+        setContentView(R.layout.activity_main_page);
     }
+
+        //Button for tips
+        public void tips(View view)
+        {
+            startActivity(new Intent(MainPage.this,Tips.class));
+        }
+
+        //Button for Maps
+        public void maps(View view)
+        {
+            startActivity(new Intent(MainPage.this, Map.class));
+        }
+
+        //Button for ResourcesActivity
+        public void resources(View view)
+        {
+            startActivity(new Intent(MainPage.this, ResourcesActivity.class));
+        }
+
+        //Button for Student Life
+        public void studentlife(View view)
+        {
+            startActivity(new Intent(MainPage.this, Student.class));
+        }
 
 }
