93a71b9f2ecce03d34630b4109e631f081626f6b
sjaime97 <38027776+sjaime97@users.noreply.github.com>
2018-05-10 17:10:41 -0700

    Update tips.java

--- app/src/main/java/com/sjsuapp/maryammostafavi/freshmanapp/tips.java
+++ app/src/main/java/com/sjsuapp/maryammostafavi/freshmanapp/tips.java
@@ -1,18 +1,29 @@
-/**
- * Created by maryammostafavi on 2/15/18.
- */
+package com.example.samanthajaime.freshmanapp;
 
-public class Tips {
-    public static void main(String[] args){
-        System.out.println("Use ratemyprofessor.com" + " but go with an open mind, " +
-                "+the professor might have bad ratings, but you might end up liking them! ");
-        System.out.println("Plan out your schedule according to your professors' office hours," +
-                "+ whether you commute or not, break time, and have a break so your brain won't fry!!");
-        System.out.println("Make sure you check out the wellness center, " +
-                "+for medical and sexual concerns, they give out free stuff!!");
-        System.out.println("Don't fill out your financial aid last minute," +
-                "+ and if you want to receive State University Grant apply before March 2nd!");
-        System.out.println("Check your MySJSU and Canvas frequently");
-        //System.out.println(
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
+
+public class Tips extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tips);
+
+        setTitle("Tips");
+        setContentView(R.layout.tips);
     }
+
+    //Button to go back to Main Page
+    public void back(View view)
+    {
+        startActivity(new Intent(Tips.this,MainPage.class));
+    }
+
 }
+
