1360420080f15f58bb9b14693448f0a77ba1b98a
sjaime97 <38027776+sjaime97@users.noreply.github.com>
2018-05-10 17:09:04 -0700

    Update Resources.java

--- app/src/main/java/com/sjsuapp/maryammostafavi/freshmanapp/Resources.java
+++ app/src/main/java/com/sjsuapp/maryammostafavi/freshmanapp/Resources.java
@@ -1,30 +1,42 @@
-package com.sjsuapp.maryammostafavi.freshmanapp;
+package com.example.samanthajaime.freshmanapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class Resources extends AppCompatActivity {
-    
+
+public class ResourcesActivity extends AppCompatActivity
+{
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Resources);
-       
-       TextView text1 = (TextView) findViewById(R.id.textView1);
-       text1.setMovementMethod(LinkMovementMethod.getInstance());
-       TextView text2 = (TextView) findViewById(R.id.textView2);
-       text2.setMovementMethod(LinkMovementMethod.getInstance());
-       TextView text3 = (TextView) findViewById(R.id.textView3);
-       text3.setMovementMethod(LinkMovementMethod.getInstance());
-       TextView text4 = (TextView) findViewById(R.id.textView4);
-       text4.setMovementMethod(LinkMovementMethod.getInstance());
+        setContentView(R.layout.resources);
 
+        setTitle("Resources For Students");
+        setContentView(R.layout.resources);
 
-      
+        TextView text = (TextView) findViewById(R.id.textView4);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text1 = (TextView) findViewById(R.id.textView5);
+        text1.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text2 = (TextView) findViewById(R.id.textView6);
+        text2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text3 = (TextView) findViewById(R.id.textView7);
+        text3.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
 
+    //Button to go back to Main Page
+    public void resources(View view)
+    {
+        startActivity(new Intent(ResourcesActivity.this, MainPage.class));
+    }
 }
